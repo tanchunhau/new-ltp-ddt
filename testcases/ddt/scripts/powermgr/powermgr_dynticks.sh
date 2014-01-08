799861c6c414adad874e43a0682a2f7ac90afc57
Carlos Hernandez <ceh@ti.com>
2014-01-08 16:28:52 -0500

    Minor fixes on dynticks test
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- testcases/ddt/scripts/powermgr/powermgr_dynticks.sh
+++ testcases/ddt/scripts/powermgr/powermgr_dynticks.sh
@@ -5,16 +5,15 @@ echo $VAR
 sleep 10
 VAR10=`cat /proc/interrupts | grep 'timer' | awk -F '[:I]' '{print $2}'|awk '{print $1}' |head -1`
 echo $VAR10
-EXP=`zcat /proc/config.gz | grep 'CONFIG_HZ' | awk -F '[:=]' '{print $2}'`
+EXP=`zcat /proc/config.gz | grep 'CONFIG_HZ=' | awk -F '[:=]' '{print $2}'`
 echo $EXP
-EXP=`echo "($EXP * 10)" | bc`
-EXPPER=`echo "($EXP * 0.1)" | bc` #Calculating 10% of expected increase
-#ACTPER=`expr $VAR10-$VAR`
+EXP=`echo "($EXP * 10)" | bc` # Calculating expected ticks in 10 seconds
+EXPPER=`echo "($EXP / 10)" | bc` #Calculating 10% of expected increase
 ACTPER=$(($VAR10-$VAR))
 
 echo "The difference is $ACTPER"
 
-if [ $ACTPER '<' $EXPPER ]; then
+if [ $ACTPER -lt $EXPPER ]; then
    exit 0
 fi
 exit 1
