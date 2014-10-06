2225edeb45133121d5718289d7ef7f8c9947ffed
Aparna Balasubramanian <aparnab@ti.com>
2014-10-06 13:29:13 -0500

    Fixes error in pwm backlight test script
    Backlight device was accidentally hardcoded in previous patch, the very problem
    that patch was trying to solve. Fixing it here.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/pwm/find_pwm_backlight_device.sh
+++ testcases/ddt/scripts/pwm/find_pwm_backlight_device.sh
@@ -1,5 +1,5 @@
 backlight_device=`ls /sys/class/backlight/|grep backlight`
-pwm_backlight_device=`udevadm info -a --path=/sys/class/backlight/backlight.9|grep DRIVERS|grep pwm-backlight`
+pwm_backlight_device=`udevadm info -a --path=/sys/class/backlight/$backlight_device|grep DRIVERS|grep pwm-backlight`
 #echo "PWM Backlight device is $pwm_backlight_device"
 if [ -z $pwm_backlight_device ];
 then 
