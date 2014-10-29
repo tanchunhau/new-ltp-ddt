26e103076169f1a2b3b8854a01e76efa3de51787
Carlos Hernandez <ceh@ti.com>
2014-10-29 11:44:36 -0400

    get_irq_for_iface.sh: Skip GPIO pins
    Avoid using GPIO pin interrupts as the interrupt number for the IP.
    For instance, MMC has a GPIO pin that triggers an interrupt when a
    card is inserted or removed, however that's not the main MMC irq.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/common/get_irq_for_iface.sh
+++ testcases/ddt/scripts/common/get_irq_for_iface.sh
@@ -27,7 +27,7 @@ usage()
 
 find_irq()
 {
-	NAME=`cat /proc/interrupts  | grep $NAME | head -1 | cut -d':' -f 1`
+	NAME=`cat /proc/interrupts  | grep -v gpio | grep $NAME | head -1 | cut -d':' -f 1`
 }
 
 find_irq_based_on_device_tree()
