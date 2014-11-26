d774ffdf6d787737e287aaf7b045abf63ad4d862
Alejandro Hernandez <ajhernandez@ti.com>
2014-11-26 18:09:37 -0500

    ipc: Add functions for rpmsg-proto tests
    This patchs adds new functions to the ipc_functions lib, used
    to test rpmsg-proto
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/ipc/ipc_funcs.sh
+++ testcases/ddt/scripts/ipc/ipc_funcs.sh
@@ -64,6 +64,7 @@ setup_firmware()
       return 1
       ;;           
   esac
+  sync
   popd
 }
 
@@ -72,6 +73,9 @@ setup_firmware()
 rm_ipc_mods()
 {
   local __modules=(rpmsg_rpc rpmsg_proto virtio_rpmsg_bus omap_remoteproc remoteproc)
+  
+  kill_lad
+  
   for __mod in ${__modules[@]}
   do
     modprobe -r ${__mod}
@@ -129,7 +133,6 @@ rpmsg_rpc_test()
   local __f_name='_triple'
   local __multiproc_cmds=("" "" "" "")
   local __rpc_cmd='test_rpmsg_rpc'
-  local __multiproc_pids=()
   local __test_log
   local __num_match
   local __command
@@ -188,6 +191,219 @@ rpmsg_rpc_test()
         echo -e "Test passed for ${__cmd:3}"
       fi
     done
+  fi
+  
+  return $__result
+}
+
+#Function to stop the ipc lad daemon
+kill_lad()
+{
+  case $MACHINE in
+    *dra7xx-evm)
+      killall lad_dra7xx
+      ;;
+    *)
+      echo "Machine ${MACHINE} not supported"
+      return 1
+      ;;           
+  esac
+  
+  return 0
+}
+
+#Function to start the ipc lad daemon
+start_lad()
+{
+  case $MACHINE in
+    *dra7xx-evm)
+      lad_dra7xx
+      ;;
+    *)
+      echo "Machine ${MACHINE} not supported"
+      return 1
+      ;;           
+  esac
+}
+
+# Funtion to run the RPMSG-RPC test
+# Inputs:
+#   $1: The number of remote processor to use in the test
+#   $2: (Optional) number of loops. Default 5000
+#   $3: (Optional) the time in sec after which the test should be killed,
+#       this option is useful to test stability
+# Returns, 0 if the test passed, 1 if the single processor test failed,
+#          2 if the multiprocessors test failed, or 3 if both single and
+#          multiprocessor test fails
+rpmsg_proto_msgqapp_test()
+{
+  local __num_procs=$1
+  local __kill_time=0
+  local __result=0
+  local __loops=5000
+  local __multiproc_cmd=""
+  local __ipc_cmd='MessageQApp'
+  local __test_log
+  local __num_match
+  local __command
+  
+  if [ $# -gt 1 ]
+  then
+    __loops=$2
+  fi
+  if [ $# -gt 2 ]
+  then
+    __kill_time=$3
+  fi
+  
+  for __rproc in `seq 1 $__num_procs`
+  do
+    __multiproc_cmd="$__multiproc_cmd & $__ipc_cmd $__loops $__rproc"
+    __command="$__ipc_cmd $__loops $__rproc"
+    if [ $__kill_time -gt 0 ]
+    then
+      __command="${__command} & sleep $__kill_time; killall $__ipc_cmd"
+    fi
+    echo $__command
+    __test_log=$(eval $__command)
+    __num_match=$(echo -e "$__test_log" | grep -c -i 'Sample\s\+application\s\+successfully\s\+completed')
+    if [ $__num_match -ne 1 ]
+    then
+      __result=1
+      echo -e "${__test_log}\nTest failed for proc ${__rproc}"
+    else
+      echo "Test passed for proc $__rproc"
+    fi
+  done
+
+  if [ $__num_procs -gt 1 ]
+  then
+    if [ $__kill_time -gt 0 ]
+    then
+      __multiproc_cmd="$__multiproc_cmd & sleep $__kill_time; killall $__ipc_cmd"
+    fi
+    echo ${__multiproc_cmd:3}
+    __test_log=$(eval ${__multiproc_cmd:3})
+    __num_match=$(echo -e "$__test_log" | grep -i -c 'Sample\s\+application\s\+successfully\s\+completed')
+    echo -e "$__num_match processors passed..."
+    if [ $__num_match -ne $__num_procs ]
+    then
+      let "__result|=2"
+      echo -e "$__test_log\nTest failed for ${__multiproc_cmd:3}"
+    else
+      echo -e "Test passed for ${__multiproc_cmd:3}"
+    fi
+  fi
+  
+  return $__result
+}
+
+# Funtion to run the RPMSG-RPC test
+# Inputs:
+#   $1: The number of remote processor to use in the test
+#   $2: (Optional) number of loops. Default 5000
+#   $3: (Optional) payload size. Default 8
+# Returns, 0 if perf data was collected, 1 if not
+rpmsg_proto_msgqbench_test()
+{
+  local __num_procs=$1
+  local __result=0
+  local __loops=5000
+  local __payload_sz=8
+  local __multiproc_cmd=""
+  local __ipc_cmd='MessageQBench'
+  local __test_log
+  local __num_match
+  local __command
+  if [ $# -gt 1 ]
+  then
+    __loops=$2
+  fi
+  if [ $# -gt 2 ]
+  then
+    __payload_sz=$3
+  fi
+  
+  for __rproc in `seq 1 $__num_procs`
+  do
+    __multiproc_cmd="$__multiproc_cmd & $__ipc_cmd $__loops $__payload_sz $__rproc"
+    __command="$__ipc_cmd $__loops  $__payload_sz $__rproc"
+    echo $__command
+    __test_log=$(eval $__command)
+    __num_match=$(echo -e "$__test_log" | grep -c -i 'Avg\s\+round\s\+trip\s\+time:')
+    if [ $__num_match -ne 1 ]
+    then
+      __result=1
+      echo -e "${__test_log}\nTest failed for proc ${__rproc}"
+    else
+      echo "Test passed for proc $__rproc"
+      test_print_trc " ROUNDTRIP_TIME | $(echo -e "$__test_log" | grep -i 'Avg\s\+round\s\+trip\s\+time:')"
+    fi
+  done
+
+  if [ $__num_procs -gt 1 ]
+  then
+    echo ${__multiproc_cmd:3}
+    __test_log=$(eval ${__multiproc_cmd:3})
+    __num_match=$(echo -e "$__test_log" | grep -c -i 'Avg\s\+round\s\+trip\s\+time:')
+    echo -e "$__num_match processors passed..."
+    if [ $__num_match -ne $__num_procs ]
+    then
+      let "__result|=2"
+      echo -e "$__test_log\nTest failed for ${__multiproc_cmd:3}"
+    else
+      echo -e "Test passed for ${__multiproc_cmd:3}"
+      test_print_trc " ROUNDTRIP_TIME_MULTI | $(echo -e "$__test_log" | grep -i 'Avg\s\+round\s\+trip\s\+time:')"
+    fi
+  fi
+  
+  return $__result
+}
+
+# Funtion to run the RPMSG-RPC test
+# Inputs:
+#   $1: (Optional) The number of threads to use in the test. 
+#       Default 10
+#   $2: (Optional) number of loops. Default 5000
+#   $3: (Optional) the time in sec after which the test should be killed,
+# Returns, 0 if succesful, 1 otherwise
+rpmsg_proto_msgqmulti_test()
+{
+  local __kill_time=0
+  local __result=0
+  local __loops=5000
+  local __num_threads=10
+  local __ipc_cmd='MessageQMulti'
+  local __test_log
+  local __num_match
+  local __command
+  if [ $# -gt 0 ]
+  then
+    __num_threads=$1
+  fi
+  if [ $# -gt 1 ]
+  then
+    __loops=$2
+  fi
+  if [ $# -gt 2 ]
+  then
+    __kill_time=$3
+  fi
+  
+  __command="$__ipc_cmd $__num_threads $__loops"
+  if [ $__kill_time -gt 0 ]
+  then
+    __command="${__command} & sleep $__kill_time; killall $__ipc_cmd"
+  fi
+  echo $__command
+  __test_log=$(eval $__command)
+  __num_match=$(echo -e "$__test_log" | grep -c -i 'pingThreadFxn\s\+successfully\s\+completed')
+  if [ $__num_match -ne $__num_threads ]
+  then
+    __result=1
+    echo -e "${__test_log}\nTest failed..."
+  else
+    echo "Test passed..."
   fi
   
   return $__result
