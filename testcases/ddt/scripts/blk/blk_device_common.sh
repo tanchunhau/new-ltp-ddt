9c3f21cd8dc15f48dd59a30823a700ed1da326b6
Yan Liu <yan-liu@ti.com>
2015-06-29 16:52:59 -0400

    blk_device_common.sh: Modified the finding test partition logic to not skip partition with boot flag
    1) Since sometimes, storage device only has one partition and it is marked as bootable flag,
    the script skipped this partition and threw error. This patch modified the way to skip boot
    partition. It skip the partition if there is u-boot* in this partition.
    
    2) if the partition could not be mounted, we assume there is problem with this partiiton,
    the script will mkfs to this partition and mount it.
    
    3) move the logic to decide which partition util to use outside of
    finding biggest test partition.
    
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/blk/blk_device_common.sh
+++ testcases/ddt/scripts/blk/blk_device_common.sh
@@ -15,7 +15,6 @@
 # Functions related to block device partition and its size 
 
 source "common.sh"
-
 ############################ Functions ################################
 
 # Create three partitions on blk device like emmc, usbhost, msata/sata etc if there
@@ -82,20 +81,9 @@ get_sector_size() {
     echo $sector_size
 }
 
-# This function return DEVNODE with the biggest size.
-#   If the partition is boot or rootfs partition, it will be skipped.
-# Input: DEV_BASE_NODE: like /dev/mmcblk0 etc
-#        device_type: like 'mmc', 'usb'
-# It only tested on MMCSD and it should work on other block devices
-#   like usb, ata etc.
-
-find_part_with_biggest_size() {
-  if [ $# -ne 2 ]; then
-    die "Usage: $0 <dev_base_node like /dev/mmcblk0, /dev/sda> <device_type like mmc, usb>"
-  fi
-
+get_part_util_to_use()
+{
   DEV_BASE_NODE=$1
-  DEVICE_TYPE=$2
 
   # check if fdisk works for this dev
   fdisk -l "$DEV_BASE_NODE" > fdisklog_$$ 2>&1 
@@ -111,6 +99,26 @@ find_part_with_biggest_size() {
     # use parted or gdisk etc
     parted -v > /dev/null && UTIL_TO_USE='parted' || (gdisk -v > /dev/null && UTIL_TO_USE='gdisk' || die "Do not know which util to check partition")
   fi
+
+  echo $UTIL_TO_USE
+}
+
+
+# This function return DEVNODE with the biggest size.
+#   If the partition is boot or rootfs partition, it will be skipped.
+# Input: DEV_BASE_NODE: like /dev/mmcblk0, /dev/sda etc
+#        DEVICE_TYPE: like 'mmc', 'usb'
+# Output: test partition which is biggest partition; but not boot or rootfs partition
+
+find_part_with_biggest_size() {
+  if [ $# -ne 2 ]; then
+    die "Usage: $0 <dev_base_node like /dev/mmcblk0, /dev/sda> <device_type like mmc, usb>"
+  fi
+
+  DEV_BASE_NODE=$1
+  DEVICE_TYPE=$2
+
+  UTIL_TO_USE=$( get_part_util_to_use $DEV_BASE_NODE ) || die "Failed to get the partition utils for $DEV_BASE_NODE : $UTIL_TO_USE "
 
   SIZE_BIGGEST=0
   TMP_IFS=$IFS
@@ -130,14 +138,12 @@ find_part_with_biggest_size() {
       DEVNODE=`echo "$LINE" | awk -F " " '{print $1}'`
       SIZE=`echo "$LINE" | awk -F " " '{print $4}' | sed s/+$//`
 
-      # skip bootable partition and rootfs partition with rootfs in it
-      if [[ "$FIELD_2ND" != '*' ]]; then
-        IS_ROOTFS=`is_part_rootfs "$DEVICE_TYPE" "$DEVNODE"` || die "error when calling is_part_rootfs: "$IS_ROOTFS" "
-        if [ "$IS_ROOTFS" == "no" ]; then
-          if [ $SIZE -gt $SIZE_BIGGEST ]; then
-            SIZE_BIGGEST=$SIZE
-            PART_DEVNODE="$DEVNODE"
-          fi
+      # skip boot partition with u-boot* in it and rootfs partition with rootfs in it
+      IS_ROOTFS_OR_BOOT=`is_part_boot_or_rootfs "$DEVICE_TYPE" "$DEVNODE"` || die "error when calling is_part_boot_or_rootfs: "$IS_ROOTFS_OR_BOOT" "
+      if [ "$IS_ROOTFS_OR_BOOT" == "no" ]; then
+        if [ $SIZE -gt $SIZE_BIGGEST ]; then
+          SIZE_BIGGEST=$SIZE
+          PART_DEVNODE="$DEVNODE"
         fi
       fi
     done
@@ -167,14 +173,11 @@ find_part_with_biggest_size() {
       FLAG=`echo "$LINE" |awk -F " " '{print $7}'`
       PART_NAME=`echo "$LINE" |awk -F " " '{print $5}'`
 
-      # if the partition flag contains 'boot', skip this partition
-      if [[ "$FLAG" != *"boot"* ]] && [[ "$PART_NAME" != *"boot"* ]]; then
-        IS_ROOTFS=`is_part_rootfs "$DEVICE_TYPE" "$DEVNODE"` || die "error when calling is_part_rootfs: "$IS_ROOTFS" "
-        if [ "$IS_ROOTFS" == "no" ]; then
-          if [ $SIZE -gt $SIZE_BIGGEST ]; then
-            SIZE_BIGGEST=$SIZE
-            PART_DEVNODE="${DEVNODE}"
-          fi
+      IS_ROOTFS_OR_BOOT=`is_part_boot_or_rootfs "$DEVICE_TYPE" "$DEVNODE"` || die "error when calling is_part_boot_or_rootfs: "$IS_ROOTFS_OR_BOOT" "
+      if [ "$IS_ROOTFS_OR_BOOT" == "no" ]; then
+        if [ $SIZE -gt $SIZE_BIGGEST ]; then
+          SIZE_BIGGEST=$SIZE
+          PART_DEVNODE="${DEVNODE}"
         fi
       fi
     done
@@ -194,8 +197,7 @@ find_part_with_biggest_size() {
 
   if [ -z "$PART_DEVNODE" ]
   then
-    
-    die "Could not find the partition to test! Maybe all the existing partitions are either boot or rootfs partitions or the size is bigger than 32G. Please use fdisk to check the partitions!"
+    die "Could not find the partition to test! Maybe all the existing partitions are either boot (contains file u-boot*) or rootfs partition (contains etc or dev directory). Please check the contents of the partitions; if needed, please reformat the device to leave at least one partition for testing!"
   fi  
   echo $PART_DEVNODE
 }
@@ -224,7 +226,7 @@ get_fs_root() {
 # Check if the partition is holding the root fs
 # Input: partition devnode: like /dev/mmcblk0p1
 #        device_type: to decide which fs to try to mount
-is_part_rootfs(){
+is_part_boot_or_rootfs(){
   if [ $# -ne 2 ]; then
     die "Usage: $0 <device_type like 'mmc', 'usb'> <device_node like /dev/mmcblk0p1, /dev/sda1>"
   fi
@@ -232,18 +234,28 @@ is_part_rootfs(){
   DEV_NODE=$2
   IS_MOUNTED="no"
   RTN="no"
+
   mount |grep "$DEV_NODE" > /dev/null && IS_MOUNTED="yes"
   if [ "$IS_MOUNTED" == "yes" ];then
-    MNT_POINT=`mount |grep "$DEV_NODE" |cut -d" " -f3` 
+    MNT_POINT=`mount |grep "$DEV_NODE" |cut -d" " -f3`
   else
-    MNT_POINT="/mnt/partition_${DEVICE_TYPE}_$$"
-    do_cmd blk_device_do_mount.sh -n "$DEV_NODE" -d "$DEVICE_TYPE" -m "$MNT_POINT" > /dev/null 2>$1 
-    mount |grep "$DEV_NODE" > /dev/null && IS_MOUNTED="yes" || die "Failed to mount $DEV_NODE when checking if it is rootfs partition"
-    NEED_UMOUNT="yes"
+    # if not mounted yet, try to mount
+    MNT_POINT="/mnt/partition_$( echo $DEV_NODE |sed s'/\/dev\///' )_$$"
+    do_cmd blk_device_do_mount.sh -n "$DEV_NODE" -d "$DEVICE_TYPE" -m "$MNT_POINT" > /dev/null 2>$1
+    mount |grep "$DEV_NODE" > /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Formatting ${DEV_NODE} since it failed to mount"
+      do_cmd mkfs.vfat -F32 ${DEV_NODE}
+      do_cmd blk_device_do_mount.sh -n "$DEV_NODE" -d "$DEVICE_TYPE" -m "$MNT_POINT" > /dev/null 2>$1
+      mount |grep "$DEV_NODE" > /dev/null || die "Failed to mount $DEV_NODE when checking if it is rootfs/boot partition"
+    fi
+    NEED_UMOUNT="yes" #flag to tell if umount is needed at the end.
   fi
-  if [ -e "$MNT_POINT/etc" -a -e "$MNT_POINT/dev" ]; then
+
+  if [ -e "$MNT_POINT/etc" ] || [ -e "$MNT_POINT/dev" ] || ls $MNT_POINT/u-boot* 1>/dev/null 2>&1 ; then
     RTN="yes"
   fi
+  #If it is mounted by me, umount it.
   if [ "$NEED_UMOUNT" == "yes" ]; then
     do_cmd blk_device_umount.sh -m "$MNT_POINT" > /dev/null 2>$1
   fi
