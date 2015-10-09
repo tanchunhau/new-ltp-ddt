98d304a7a33b5300282a0383785412ee0f1a5347
Carlos Hernandez <ceh@ti.com>
2015-10-09 18:31:23 -0400

    functions.sh: Add function to run cyclictest
    The function returns pass/fail based on max latency measured and
    pass criteria passed to it.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- testcases/ddt/scripts/common/functions.sh
+++ testcases/ddt/scripts/common/functions.sh
@@ -1053,5 +1053,16 @@ join ()
 { 
 local IFS="$1"; shift; echo "$*";
 }
-  
-  
+
+# Run cyclictest and compare max latency agains $1 pass criteria
+run_cyclictest()
+{
+    local passcriteria=$1
+    shift 1
+    cyclictest $@  | awk -v passcriteria=$passcriteria '
+      BEGIN {max=0; FS="Max:"};
+      {print $0};
+      /^T:.+Max:\s+[[:digit:]]+/ {if ($2 > max) max=$2};
+      END {print "max_latency=" max};
+      END {if (max > passcriteria) {print "TEST:FAILED"; exit 1;} else {print "TEST:PASSED"; exit 0}}'
+}
