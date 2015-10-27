37906261521db19e1f4816ea4536201c4e8fa4bd
Aparna Balasubramanian <aparnab@ti.com>
2015-10-27 17:02:36 -0500

    Changes to wlan tests for right SSID and wpa_cli commands
    1) to use wpa_cli with right parameter which would be wlan0 by default.
    2) to modify testcase with the right functions.
    
    Wlan interface parameter with wpa_cli commands
    
    Adds -i wlan0 parameter to wpa_cli commands, without which scan commands
    will fail. On previous releases default parameter was wlan0 so commands
    could be used without the -i parameter. However, in 4.1 release, p2p is
    the default interface requiring specific reference to wlan0 now.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    
    Environment variable name for wlan ssid
    
    Modifying environment variable name for wlan ssid to use the same
    name being used in dut params to avoid a new dut params definition
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/wlan/wlan_station_setup.sh
+++ testcases/ddt/scripts/wlan/wlan_station_setup.sh
@@ -14,29 +14,38 @@
 
 # @desc Starts a wlan station interface by invoking sta_start.sh and other
 #	supporting scripts from /usr/share/wl18xx location.
-#       SSID is set as environment variable WILINK_WLAN_OPEN_SSID manually for      
+#       SSID is set as environment variable AP_SSID manually for      
 #       standalone LTP-DDT operation or exported from bench file when          
 #       using vatf test execution. 
 
-# @params none
+# @params <interface_name such as wlan0 or wlan1>, optional, default wlan0
 
 # @returns 0 if commands are successful
 
 ###############################################################################
 
 source "common.sh"  # Import do_cmd(), die() and other functions
-`check_env_var 'WILINK_WLAN_OPEN_SSID'`
+
+if [ ! -z $1 ] 
+then 
+   wlan_interface=$1 
+else
+   wlan_interface="wlan0"
+fi
+
+
+`check_env_var 'AP_SSID'`
 demo_folder=`find / -name sta_start.sh`
 [ -n "$demo_folder" ] || die "Unable to find sta_start.sh in filesystem. Make sure all station scripts are present." 
 demo_folder=${demo_folder%"/sta_start.sh"}
 nohup "$demo_folder"/sta_start.sh &
 sleep 5
-scan_result=`wpa_cli scan`
+scan_result=`wpa_cli -i $wlan_interface scan`
 sleep 5
-scan_result=`wpa_cli scan_r|grep $WILINK_WLAN_OPEN_SSID`
-[ -n "$scan_result" ] || die "Expected SSID $WILINK_WLAN_OPEN_SSID cannot be scanned. Check wireless router is turned on and/or check bench file"
+scan_result=`wpa_cli -i $wlan_interface scan_r|grep $AP_SSID`
+[ -n "$scan_result" ] || die "Expected SSID $AP_SSID cannot be scanned. Check wireless router is turned on and/or check bench file"
 		
 # cd into demo folder since the sta_connect-ex-dhcp.sh depends on a local script
 cd "$demo_folder"
-connect_result=`./sta_connect-ex-dhcp.sh $WILINK_WLAN_OPEN_SSID`
-[ -n "$connect_result" ] || die "Unable to connect to SSID. Check connecting manually to SSID $WILINK_WLAN_OPEN_SSID"
+connect_result=`./sta_connect-ex-dhcp.sh $AP_SSID`
+[ -n "$connect_result" ] || die "Unable to connect to SSID. Check connecting manually to SSID $AP_SSID"
