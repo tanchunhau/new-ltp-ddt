a9dda58de8bc7e79122639c6b00e9522fc0d5cb8
Aparna Balasubramanian <aparnab@ti.com>
2016-03-03 11:46:07 -0600

    Update k2g-evm for micrel eth phy type
    Add k2g-evm to list of platforms which use micrel phy and hence
    which require for tests to account for a delay for link to come up
    for these platforms.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/eth/get_eth_phy_name.sh
+++ testcases/ddt/scripts/eth/get_eth_phy_name.sh
@@ -33,7 +33,7 @@ esac
 case $SOC in
 esac
 case $MACHINE in
-        am57xx-evm|am571x-idk|am572x-idk|am43xx-gpevm)
+        am57xx-evm|am571x-idk|am572x-idk|am43xx-gpevm|k2g-evm)
                 phy="micrel";;
 esac
 echo "$phy"
