de6e1f0c225da74bd4e081d66aeeb33814eba78c
Aparna Balasubramanian <aparnab@ti.com>
2016-06-28 17:10:01 -0500

    Modify interrupt source to be tracked for crypto tests
    Due to changes in the interface being used for crypto device nodes,
    the following changes are being made in order to checking interrupt
    count to confirm hardware crypto.
    
    1) Modify default crypto interrupt counts to be tracked on edma
    
    2) Only des and des3 use omap-dma-controller. Hence, make the corresponding
    change in get_crypto_ip.sh.
    
    3) Add an optional argument for get_crypto_ip.sh due to the above des requirement.
    
    4) Make changes to test cases for des and 3des to use this new parameter.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/common/get_crypto_ip.sh
+++ testcases/ddt/scripts/common/get_crypto_ip.sh
@@ -1,10 +1,11 @@
 source "common.sh"                                                      
                                                                         
 ############################### CLI Params ###################################
-                                                                              
+ip_type=$1                                                                              
 ############################ USER-DEFINED Params ##############################
 # Try to avoid defining values here, instead see if possible                   
 # to determine the value dynamically                                           
+CRYPTO_IP=''
 case $ARCH in                                                        
 esac                                                                 
 case $DRIVER in                                                      
@@ -12,10 +13,12 @@ esac
 case $SOC in                                  
 esac              
 case $MACHINE in                                                              
-am335x-evm|am335x-sk|beaglebone|beaglebone-black|am43xx-epos|am43xx-gpevm)
-  CRYPTO_IP='edma';;
+esac
+case $ip_type in                                                              
+des|3des)
+	CRYPTO_IP='omap-dma-engine';;
 *)
-  CRYPTO_IP='omap-dma-engine';;
+	CRYPTO_IP='edma';;
 esac                                                                          
 
 echo $CRYPTO_IP
