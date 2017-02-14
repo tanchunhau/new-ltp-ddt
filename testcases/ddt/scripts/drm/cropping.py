892fc2145847a083e442dc456add0de2e97739b8
Alejandro Hernandez <ajhernandez@ti.com>
2017-02-14 17:12:01 -0500

    Show unmodified buffer in cropping test
    This patch adds code to the cropping.py test script in order
    to show the unmodified buffer first before showing the cropped
    buffers. Doing so allows the user to observe the area being
    cropped
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- testcases/ddt/scripts/drm/cropping.py
+++ testcases/ddt/scripts/drm/cropping.py
@@ -31,8 +31,9 @@ offset = int(h/4)
 fbs=[]
 
 for i in range(len(planes)):
-    fbs.append(pykms.OmapFramebuffer(card, w, h, pykms.PixelFormat.ARGB8888))
-    pykms.draw_rect(fbs[i], i*offset, i*offset, w/2, h/2, pykms.RGB(128, 255*(1 & 2**i), 255*((2 & 2**i) >> i), 255*((4 & 2**i) >> i)))
+    fbs.append(pykms.OmapFramebuffer(card, w, h, pykms.PixelFormat.RGB888))
+    pykms.draw_rect(fbs[i], 0, 0, w, h, pykms.RGB(255*(i==0), 255*(i==1), 255*(i==2)))
+    pykms.draw_rect(fbs[i], i*offset, i*offset, w/2**(i+1), h/2**(i+1), pykms.RGB(255*(i!=1), 255*(i!=2), 255*(i!=0)))
 
 crtc.set_props({
     "trans-key-mode": 0,
@@ -41,6 +42,29 @@ crtc.set_props({
     "alpha_blender": 1,
 })
 
+#Show the whole frame
+for i in range(len(planes)):
+    plane = planes[i]
+    fb = fbs[i]
+
+    print("set crtc {}, plane {}, fb {} ({}, {})".format(crtc.id, plane.id, fbs[i].id, fb.width, fb.height))
+
+    plane.set_props({
+        "FB_ID": fb.id,
+        "CRTC_ID": crtc.id,
+        "SRC_X": 0,
+        "SRC_Y": 0,
+        "SRC_W": fb.width << 16,
+        "SRC_H": fb.height << 16,
+        "CRTC_X": 0,
+        "CRTC_Y": 0,
+        "CRTC_W": fb.width,
+        "CRTC_H": fb.height,
+        "zorder": i,
+    })
+    time.sleep(1)
+
+#Show only the cropped rect
 for i in range(len(planes)):
     plane = planes[i]
     fb = fbs[i]
