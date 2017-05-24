9e6041fa7e576401934900e2276fa9357be321f2
Alejandro Hernandez <ajhernandez@ti.com>
2017-05-24 12:10:04 -0400

    drm: Update zorder test to account for new planes available
    Now that all planes not being used are available for
    a CRTC, update zorder test to accomodate any new
    planes available in a board
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/drm/zorder.py
+++ testcases/ddt/scripts/drm/zorder.py
@@ -24,7 +24,8 @@ card.disable_planes()
 
 w = mode.hdisplay
 h = mode.vdisplay
-offset = int(h/4)
+offset = int(h/2**(len(planes)-1))
+l_div = len(planes) - 1
 
 #print "width = {}, height = {}".format(w,h)
 
@@ -32,7 +33,7 @@ fbs=[]
 
 for i in range(len(planes)):
     fbs.append(pykms.DumbFramebuffer(card, w, h, pykms.PixelFormat.RGB888))
-    pykms.draw_rect(fbs[i], 0, 0, int(w/2), int(h/2), pykms.RGB(255*(1 & 2**i), 255*((2 & 2**i) >> i), 255*((4 & 2**i) >> i)))
+    pykms.draw_rect(fbs[i], 0, 0, int(w/l_div), int(h/l_div), pykms.RGB(255*(1 & 2**(i%3)), 255*((2 & 2**(i%3)) >> i), 255*((4 & 2**(i%3)) >> i)))
 
 crtc.set_props({
     "trans-key-mode": 0,
@@ -52,12 +53,12 @@ for i in range(len(planes)):
         "CRTC_ID": crtc.id,
         "SRC_X": 0,
         "SRC_Y": 0,
-        "SRC_W":  int(w/2) << 16,
-        "SRC_H":  int(h/2) << 16,
+        "SRC_W":  int(w/l_div) << 16,
+        "SRC_H":  int(h/l_div) << 16,
         "CRTC_X": i*offset,
         "CRTC_Y": i*offset,
-        "CRTC_W": int(w/2),
-        "CRTC_H": int(h/2),
+        "CRTC_W": int(w/l_div),
+        "CRTC_H": int(h/l_div),
         "zorder": i,
     })
 
