0da9ea13f6e2cef3165da391650352fac5c241bb
Aparna Balasubramanian <aparnab@ti.com>
2017-08-22 16:42:06 -0500

    False failure checks to Crypto tests
    Presence of a SATA or any other storage device which results in
    kernel errors causes the crypto tests to fail because those
    failure conditions have not been treated as false (unrelated
    to crypto tests). Adding such errors to the to-be-ignored list
    will reduce number of false failures.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/crypto/crypto_test.sh
+++ testcases/ddt/scripts/crypto/crypto_test.sh
@@ -19,6 +19,8 @@ do
                     *"rfc3686"*) echo "RFC3686 failure";;
                     *"cts"*) echo "CTS failure";;
                     *"request size is not exact amount"*) echo "Input padding failure";;
+                    *"ata"*) echo "Kernel error reported from a connected ATA device";;
+                    *"dev sd"*) echo "Kernel error reported from a connected sd* device";;
                     *) echo "TRUE failure"
                        echo $LINE
                        fail_flag=1
