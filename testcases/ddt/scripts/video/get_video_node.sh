f76f614f7bfd15f4b558dad51eb532e11759f274
Alejandro Hernandez <ajhernandez@ti.com>
2017-12-05 10:08:42 -0500

    Add v4l2 device specific compliance tests
    This patch adds testcases to check v4l2 compliance for VPE,
    VIP, VPFE, CAL and VPIF devices
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/video/get_video_node.sh
+++ testcases/ddt/scripts/video/get_video_node.sh
@@ -17,75 +17,42 @@
 # @returns Display/capture device node
 # @history 2011-03-05: First version
 
+# Refactor script to allow filtering for the type of video device (vpe, vip, etc)
+# @history 2017-12-05: First version
+
 source "common.sh"  # Import do_cmd(), die() and other functions
 
 ############################# Functions #######################################
 usage()
 {
 cat <<-EOF >&1
-        usage: ./${0##*/} [-d DISPLAY] 
-        -d DISPLAY  	y/n depending on whether a display or capture device driver node is required
+        usage: ./${0##*/} [-d TYPE]
+        -d TYPE      (Optional) type of video device to filter for
         -h Help      print this usage
 EOF
 exit 0
 }
 
 ############################ Script Variables ##################################
-DISPLAY="y"
+TYPE=".*"
 
 ################################ CLI Params ####################################
-# Please use getopts
 while getopts  :d:h arg
 do case $arg in
-	 d)      DISPLAY="$OPTARG";;
+        d)      TYPE="$OPTARG";;
         h)      usage;;
-        :)      die "$0: Must supply an argument to -$OPTARG.";; 
         \?)     die "Invalid Option -$OPTARG ";;
 esac
 done
 
-############################ USER-DEFINED Params ###############################
-# Try to avoid defining values here, instead see if possible
-# to determine the value dynamically. ARCH, DRIVER, SOC and MACHINE are 
-# initilized and exported by runltp script based on platform option (-P)
-
-# Define default values for variables being overriden
-
-case $ARCH in
-esac
-case $DRIVER in
-esac
-case $SOC in	
-esac
-case $MACHINE in	
-esac
 
 ########################### DYNAMICALLY-DEFINED Params #########################
-# Try to use /sys and /proc information to determine values dynamically.
-# Alternatively you should check if there is an existing script to get the
-# value you want
 
 ########################### REUSABLE TEST LOGIC ###############################
-# The driver names are for omap3.
-NODE=0
-for devicenode in $(echo `ls /sys/class/video4linux/video?/name`)
-do
 
-        DRIVER=`cat /sys/class/video4linux/video$NODE/name`
-	 if [ "$DISPLAY" == "y" ];then
-        	 if [ "$DRIVER" == "omap_vout" ];then
-              	  DEVICE_DISP="/dev/video$NODE"
-	                echo "$DEVICE_DISP"
-			  exit 0
-		 fi
-        else
-		 if [ "$DRIVER" == "OMAP3 ISP CCDC output" ];then
-		 	  DEVICE_CAP="/dev/video$NODE"
-             		  echo "$DEVICE_CAP"
-			  exit 0
-	        fi
-	 fi
-        NODE=$((NODE+1))
+for devicenode in `ls /sys/class/video4linux/ | grep video`
+do
+        cat /sys/class/video4linux/${devicenode}/name | grep -i -q "${TYPE}" && echo $devicenode
 done
 
 
