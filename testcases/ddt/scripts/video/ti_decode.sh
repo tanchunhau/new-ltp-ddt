9a438bed1c2cbc1952f23ab8337d0341621fc45f
Alejandro Hernandez <ajhernandez@ti.com>
2019-06-28 17:50:02 -0400

    tidec: Check and fetch test media
    Add a function to check if the media used for the decode
    tests is in the fs and fetch it when the media is not
    found.
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/video/ti_decode.sh
+++ testcases/ddt/scripts/video/ti_decode.sh
@@ -31,3 +31,37 @@ run_tidec_decode()
 	insert_video_modules
 	tidec_decode -b $* | grep 'test app completed successfully'
 }
+
+# Download Test media if not in fs
+get_media()
+{
+	local __media_url=http://gtopentest-server.gt.design.ti.com/anonymous/common/Multimedia/ti-dec-examples/testvecs
+	local __checksums=/tmp/checksums.txt
+	local __media_folder=/usr/share/ti/tidec-decode
+	local __media
+
+	OPTIND=1
+	while getopts ":s:" arg
+	do
+		case $arg in
+			s)  __=( $OPTARG )
+			;;
+			\?) echo "ignoring option $OPTARG" >&2 #ignore
+				shift 1
+			;;
+		esac
+	done
+	ls ${__media_folder} &>/dev/null || mkdir -p ${__media_folder}
+	local __media_checksum=$(md5sum /usr/share/ti/tidec-decode/* | awk '{print $1}' | sort -u)
+	Wget ${__media_url}/media_checksums.txt -O ${__checksums} || return 1
+	local __remote_checksum=$(awk '{print $1}' ${__checksums} | sort -u)
+	local __remote_media=$(awk '{print $2}' ${__checksums} | sort -u)
+	if [[ "$__media_checksum" != "$__remote_checksum" ]]
+	then
+		rm ${__media_folder}/*
+		for __media in $__remote_media
+		do
+		    Wget ${__media_url}/${__media} -O ${__media_folder}/`basename ${__media}` || echo "Could not download  ${__media_url}/${__media}"
+		done
+	fi
+}
