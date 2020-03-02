tanchunhau/new-ltp-ddt

commit 714a81d464bcfd96d0eff11f7af64b41ba51904a

uart_hwflow: Run test only on a particular port
In case of external loopback tests, not all instances of UART are pinned
out and not all instances are on headers where loopback can be done.
Therefore provide a way to get port per SOC and run hwflow flow control
only on that instance.

For J721e, this port is /dev/ttyS6. And hwflow tests now run only on HW
flow control.

Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
Signed-off-by: Carlos Hernandez <ceh@ti.com>

runtest/ddt/uart_hwflow
@@ -5,25 +5,25 @@
 # @requires
 # @setup_requires uarthwflow
 
-UART_S_FUNC_9600_HWFLOW source 'serialcheck.sh' -r 9600 -x
-UART_S_FUNC_19200_HWFLOW source 'serialcheck.sh' -r 19200 -x
-UART_S_FUNC_38400_HWFLOW source 'serialcheck.sh' -r 38400 -x
-UART_S_FUNC_57600_HWFLOW source 'serialcheck.sh' -r 57600 -x
-UART_XS_FUNC_115200_HWFLOW source 'serialcheck.sh' -r 115200 -x
-UART_L_STRESS_115200_HWFLOW source 'serialcheck.sh' -r 115200 -l 200 -x
-UART_S_FUNC_230400_HWFLOW source 'serialcheck.sh' -r 230400 -x
-UART_S_FUNC_460800_HWFLOW source 'serialcheck.sh' -r 460800 -x
-UART_S_FUNC_500000_HWFLOW source 'serialcheck.sh' -r 500000 -x
-UART_S_FUNC_576000_HWFLOW source 'serialcheck.sh' -r 576000 -x
-UART_S_FUNC_921600_HWFLOW source 'serialcheck.sh' -r 921600 -x
-UART_S_FUNC_1000000_HWFLOW source 'serialcheck.sh' -r 1000000 -x
-UART_S_FUNC_1152000_HWFLOW source 'serialcheck.sh' -r 1152000 -x
-UART_S_FUNC_1500000_HWFLOW source 'serialcheck.sh' -r 1500000 -x
-UART_S_FUNC_2000000_HWFLOW source 'serialcheck.sh' -r 2000000 -x
-UART_S_FUNC_2500000_HWFLOW source 'serialcheck.sh' -r 2500000 -x
-UART_S_FUNC_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -x
-UART_S_PERF_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -l 50 -x
-UART_S_FUNC_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -x
-UART_L_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 500 -x
-UART_XL_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 5000 -x
-UART_S_FUNC_4000000_HWFLOW source 'serialcheck.sh' -r 4000000 -x
+UART_S_FUNC_9600_HWFLOW source 'serialcheck.sh' -r 9600 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_19200_HWFLOW source 'serialcheck.sh' -r 19200 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_38400_HWFLOW source 'serialcheck.sh' -r 38400 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_57600_HWFLOW source 'serialcheck.sh' -r 57600 -d `get_uart_test_port.sh` -x
+UART_XS_FUNC_115200_HWFLOW source 'serialcheck.sh' -r 115200 -d `get_uart_test_port.sh` -x
+UART_L_STRESS_115200_HWFLOW source 'serialcheck.sh' -r 115200 -l 200 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_230400_HWFLOW source 'serialcheck.sh' -r 230400 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_460800_HWFLOW source 'serialcheck.sh' -r 460800 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_500000_HWFLOW source 'serialcheck.sh' -r 500000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_576000_HWFLOW source 'serialcheck.sh' -r 576000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_921600_HWFLOW source 'serialcheck.sh' -r 921600 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_1000000_HWFLOW source 'serialcheck.sh' -r 1000000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_1152000_HWFLOW source 'serialcheck.sh' -r 1152000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_1500000_HWFLOW source 'serialcheck.sh' -r 1500000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_2000000_HWFLOW source 'serialcheck.sh' -r 2000000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_2500000_HWFLOW source 'serialcheck.sh' -r 2500000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -d `get_uart_test_port.sh` -x
+UART_S_PERF_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -l 50 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -d `get_uart_test_port.sh` -x
+UART_L_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 500 -d `get_uart_test_port.sh` -x
+UART_XL_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 5000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_4000000_HWFLOW source 'serialcheck.sh' -r 4000000 -d `get_uart_test_port.sh` -x

testcases/ddt/scripts/uart/get_uart_test_port.sh
@@ -1,0 +1,33 @@
+#!/bin/sh
+#
+# Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 2.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any
+# kind, whether express or implied; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# Get UART PORT that has external loopback connection.
+
+#source "common.sh"
+
+############################### CLI Params ###################################
+
+############################ USER-DEFINED Params ##############################
+# Try to avoid defining values here, instead see if possible
+# to determine the value dynamically
+case $ARCH in
+esac
+case $DRIVER in
+esac
+case $SOC in
+j721e)
+	echo "/dev/ttyS6"
+esac
+case $MACHINE in
+esac

commit f44b40484ad6ad1d61dcbb52e4bcfbcb715f2286

uart: Fix bug introduced by 714a81d46
After 714a81d46, the tests could only be run on j7 devices as dev
serial node was set to blank for all other devices.
Revert the logic and set specific dev serial node only for j7.

Signed-off-by: Carlos Hernandez <ceh@ti.com>

runtest/ddt/uart_hwflow
@@ -5,25 +5,25 @@
 # @requires
 # @setup_requires uarthwflow
 
-UART_S_FUNC_9600_HWFLOW source 'serialcheck.sh' -r 9600 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_19200_HWFLOW source 'serialcheck.sh' -r 19200 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_38400_HWFLOW source 'serialcheck.sh' -r 38400 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_57600_HWFLOW source 'serialcheck.sh' -r 57600 -d `get_uart_test_port.sh` -x
-UART_XS_FUNC_115200_HWFLOW source 'serialcheck.sh' -r 115200 -d `get_uart_test_port.sh` -x
-UART_L_STRESS_115200_HWFLOW source 'serialcheck.sh' -r 115200 -l 200 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_230400_HWFLOW source 'serialcheck.sh' -r 230400 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_460800_HWFLOW source 'serialcheck.sh' -r 460800 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_500000_HWFLOW source 'serialcheck.sh' -r 500000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_576000_HWFLOW source 'serialcheck.sh' -r 576000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_921600_HWFLOW source 'serialcheck.sh' -r 921600 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_1000000_HWFLOW source 'serialcheck.sh' -r 1000000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_1152000_HWFLOW source 'serialcheck.sh' -r 1152000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_1500000_HWFLOW source 'serialcheck.sh' -r 1500000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_2000000_HWFLOW source 'serialcheck.sh' -r 2000000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_2500000_HWFLOW source 'serialcheck.sh' -r 2500000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -d `get_uart_test_port.sh` -x
-UART_S_PERF_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -l 50 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -d `get_uart_test_port.sh` -x
-UART_L_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 500 -d `get_uart_test_port.sh` -x
-UART_XL_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 5000 -d `get_uart_test_port.sh` -x
-UART_S_FUNC_4000000_HWFLOW source 'serialcheck.sh' -r 4000000 -d `get_uart_test_port.sh` -x
+UART_S_FUNC_9600_HWFLOW source 'serialcheck.sh' -r 9600 -x
+UART_S_FUNC_19200_HWFLOW source 'serialcheck.sh' -r 19200 -x
+UART_S_FUNC_38400_HWFLOW source 'serialcheck.sh' -r 38400 -x
+UART_S_FUNC_57600_HWFLOW source 'serialcheck.sh' -r 57600 -x
+UART_XS_FUNC_115200_HWFLOW source 'serialcheck.sh' -r 115200 -x
+UART_L_STRESS_115200_HWFLOW source 'serialcheck.sh' -r 115200 -l 200 -x
+UART_S_FUNC_230400_HWFLOW source 'serialcheck.sh' -r 230400 -x
+UART_S_FUNC_460800_HWFLOW source 'serialcheck.sh' -r 460800 -x
+UART_S_FUNC_500000_HWFLOW source 'serialcheck.sh' -r 500000 -x
+UART_S_FUNC_576000_HWFLOW source 'serialcheck.sh' -r 576000 -x
+UART_S_FUNC_921600_HWFLOW source 'serialcheck.sh' -r 921600 -x
+UART_S_FUNC_1000000_HWFLOW source 'serialcheck.sh' -r 1000000 -x
+UART_S_FUNC_1152000_HWFLOW source 'serialcheck.sh' -r 1152000 -x
+UART_S_FUNC_1500000_HWFLOW source 'serialcheck.sh' -r 1500000 -x
+UART_S_FUNC_2000000_HWFLOW source 'serialcheck.sh' -r 2000000 -x
+UART_S_FUNC_2500000_HWFLOW source 'serialcheck.sh' -r 2500000 -x
+UART_S_FUNC_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -x
+UART_S_PERF_3000000_HWFLOW source 'serialcheck.sh' -r 3000000 -l 50 -x
+UART_S_FUNC_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -x
+UART_L_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 500 -x
+UART_XL_STRESS_3500000_HWFLOW source 'serialcheck.sh' -r 3500000 -l 5000 -x
+UART_S_FUNC_4000000_HWFLOW source 'serialcheck.sh' -r 4000000 -x

testcases/ddt/scripts/uart/get_uart_test_port.sh
@@ -1,33 +1,0 @@
-#!/bin/sh
-#
-# Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation version 2.
-#
-# This program is distributed "as is" WITHOUT ANY WARRANTY of any
-# kind, whether express or implied; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-
-# Get UART PORT that has external loopback connection.
-
-#source "common.sh"
-
-############################### CLI Params ###################################
-
-############################ USER-DEFINED Params ##############################
-# Try to avoid defining values here, instead see if possible
-# to determine the value dynamically
-case $ARCH in
-esac
-case $DRIVER in
-esac
-case $SOC in
-j721e)
-	echo "/dev/ttyS6"
-esac
-case $MACHINE in
-esac

testcases/ddt/scripts/uart/serialcheck.sh
@@ -89,6 +89,17 @@ done
 : ${UART_LOOPS:=5}
 : ${UART_HWFLOW:=0}
 
+case $ARCH in
+esac
+case $DRIVER in
+esac
+case $SOC in
+j721e)
+	PORT="/dev/ttyS6"
+esac
+case $MACHINE in
+esac
+
 PORTS_TO_TEST=();
 UART_PORTS=();
 ARRAY=(`find /sys/class/tty/*/uartclk`);