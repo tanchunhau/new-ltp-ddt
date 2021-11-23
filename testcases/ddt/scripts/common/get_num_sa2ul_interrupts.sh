15a7adca304402828c5ddc49c2a160b5d62df56d
Jayesh Choudhary <j-choudhary@ti.com>
2021-11-23 10:56:24 +0530

    Crypto: add more channel in arr for interrupt grep
    chan 6-39 added in arr declaration for gerp of the interrupt count
    
    Fixes: LCPD-24429
    
    Signed-off-by: Jayesh Choudhary <j-choudhary@ti.com>

--- testcases/ddt/scripts/common/get_num_sa2ul_interrupts.sh
+++ testcases/ddt/scripts/common/get_num_sa2ul_interrupts.sh
@@ -50,7 +50,7 @@ if [ "x$CPU_NUM" == "x" ]; then
 fi
 INT_NUM=0
 TOTAL_INT_NUM=0
-declare -a arr=("chan0" "chan1" "chan2" "chan3" "chan4" "chan5")
+declare -a arr=("chan0" "chan1" "chan2" "chan3" "chan4" "chan5" "chan6" "chan7" "chan8" "chan9" "chan10" "chan11" "chan12" "chan13" "chan14" "chan15" "chan16" "chan17" "chan18" "chan19" "chan20" "chan21" "chan22" "chan23" "chan24" "chan25" "chan26" "chan27" "chan28" "chan29" "chan30" "chan31" "chan32" "chan33" "chan34" "chan35" "chan36" "chan37" "chan38" "chan39")
 for i in "${arr[@]}"
 do
    IRQ_NUM=`cat /proc/interrupts | grep -i "dma-controller" | grep $i| head -n 1 | tail -n 1| cut -d':' -f 1`
