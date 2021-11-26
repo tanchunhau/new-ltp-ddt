31ee446220c419471c7f7ee7b73b0523bb024618
Jayesh Choudhary <j-choudhary@ti.com>
2021-11-26 16:32:29 +0530

    Crypto: change logic for interrupt grep for different platforms
    modify logic to count the number of dma-controller chan for
    a platform and then count the interrupts for those chan.
    
    Fixes: LCPD-24429 , LCPD-24600
    
    Signed-off-by: Jayesh Choudhary <j-choudhary@ti.com>

--- testcases/ddt/scripts/common/get_num_sa2ul_interrupts.sh
+++ testcases/ddt/scripts/common/get_num_sa2ul_interrupts.sh
@@ -50,13 +50,17 @@ if [ "x$CPU_NUM" == "x" ]; then
 fi
 INT_NUM=0
 TOTAL_INT_NUM=0
-declare -a arr=("chan0" "chan1" "chan2" "chan3" "chan4" "chan5" "chan6" "chan7" "chan8" "chan9" "chan10" "chan11" "chan12" "chan13" "chan14" "chan15" "chan16" "chan17" "chan18" "chan19" "chan20" "chan21" "chan22" "chan23" "chan24" "chan25" "chan26" "chan27" "chan28" "chan29" "chan30" "chan31" "chan32" "chan33" "chan34" "chan35" "chan36" "chan37" "chan38" "chan39")
-for i in "${arr[@]}"
+INT_CHAN=`cat /proc/interrupts | grep -i "Level" | grep -c "dma-controller"`
+((INT_CHAN=INT_CHAN-1))
+for i in $(seq 0 $INT_CHAN)
 do
-   IRQ_NUM=`cat /proc/interrupts | grep -i "dma-controller" | grep $i| head -n 1 | tail -n 1| cut -d':' -f 1`
-   interrupts=`cat /proc/interrupts | grep "${IRQ_NUM}:" | cut -d':' -f 2`
-   INT_NUM=`echo $interrupts | cut -d' ' -f $(( $CPU_NUM + 1 ))`
-   TOTAL_INT_NUM=`expr $TOTAL_INT_NUM + $INT_NUM`
+   IRQ_NUM=`cat /proc/interrupts | grep -i "dma-controller" | grep "chan$i" | head -n 1 | tail -n 1| cut -d':' -f 1`
+   if [ $IRQ_NUM ]
+   then
+     interrupts=`cat /proc/interrupts | grep "${IRQ_NUM}:" | cut -d':' -f 2`
+     INT_NUM=`echo $interrupts | cut -d' ' -f 1`
+     TOTAL_INT_NUM=`expr $TOTAL_INT_NUM + $INT_NUM`
+   fi
 done
 ## Return number of interrupts
 echo $TOTAL_INT_NUM
