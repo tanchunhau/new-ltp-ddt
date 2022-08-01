1e62376a4ccc7b7b47e0fcf3a0f412e4c76cc60c
vincent-teh <dawnogaming01@gmail.com>
2022-08-01 11:33:41 +0800

    revert gpiod.c changes

--- testcases/starfive/gpiod/src/testcases/gpiod.c
+++ testcases/starfive/gpiod/src/testcases/gpiod.c
@@ -7,7 +7,6 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <st_log.h>
 
 #ifndef CONSUMER
 #define CONSUMER    "YuSheng"
@@ -31,7 +30,7 @@ int main(int argc, char const *argv[])
 	struct gpiod_chip *chip;
 	struct gpiod_line *line1;
     struct gpiod_line *line2;
-    int i, ret, iret1, iret2;
+    int i, ret = 0, iret1, iret2;
 
     chip = gpiod_chip_open_by_name(chipname);
 	if (!chip) {
@@ -93,8 +92,6 @@ release_line1:
 close_chip:
     gpiod_chip_close(chip);
 end:
-    TEST_PRINT_TST_RESULT(ret, testcaseid);
-	TEST_PRINT_TST_END(testcaseid);
     return ret;
 }
 
