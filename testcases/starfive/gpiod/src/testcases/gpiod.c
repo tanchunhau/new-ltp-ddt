b359ea24ca3c29f20af8933f65bb721ce12fc497
vincent-teh <dawnogaming01@gmail.com>
2022-08-03 15:39:47 +0800

    debugging st_log.h func

--- testcases/starfive/gpiod/src/testcases/gpiod.c
+++ testcases/starfive/gpiod/src/testcases/gpiod.c
@@ -130,7 +130,6 @@ release_line1:
 close_chip:
     gpiod_chip_close(chip);
 end:
-    TEST_PRINT_TST_RESULT(result, testcaseid);
     TEST_PRINT_TST_END(testcaseid);
     return ret;
 }
@@ -200,6 +199,7 @@ void *detect_pwm( void *ptr ){
         if (pwm_num == 10 && counter == 10){
             TEST_PRINT_ERR("10 signal detected successfully\n");
             result = 0;
+            TEST_PRINT_TST_RESULT(result, testcaseid);
             break;
         }
         else if (pwm_num == 10 && counter != 10)
